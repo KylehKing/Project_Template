aa49da08c172c9e66a4afc07fd28b4f75fea9d23
KylehKing <kyle.hocking@hotmail.com>
2025-02-06 15:29:47 +0000

    Add project files.

---
+++ scenebasic_uniform.h
@@ -0,0 +1,32 @@
+#ifndef SCENEBASIC_UNIFORM_H
+#define SCENEBASIC_UNIFORM_H
+
+#include "helper/scene.h"
+
+#include <glad/glad.h>
+#include "helper/glslprogram.h"
+#include "helper/torus.h"
+#include <glm/glm.hpp>
+
+#include <glm/ext/matrix_transform.hpp>
+
+class SceneBasic_Uniform : public Scene
+{
+private:
+    Torus torus;
+    GLSLProgram prog;
+    void setMatrices();
+
+
+    void compile();
+
+public:
+    SceneBasic_Uniform();
+
+    void initScene();
+    void update( float t );
+    void render();
+    void resize(int, int);
+};
+
+#endif // SCENEBASIC_UNIFORM_H
