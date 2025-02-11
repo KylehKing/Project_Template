56dbfceb2b2091640b4fe4b8a53d2f91137a77cf
KylehKing <kyle.hocking@hotmail.com>
2025-02-11 18:36:35 +0000

    Lab 3 part 1

--- scenebasic_uniform.cpp
+++ scenebasic_uniform.cpp
@@ -6,6 +6,7 @@
 #include <string>
 using std::string;
 
+#include <sstream>
 #include <iostream>
 using std::cerr;
 using std::endl;
@@ -27,15 +28,25 @@ void SceneBasic_Uniform::initScene()
 	model = glm::rotate(model, glm::radians(-35.0f), vec3(1.0f, 0.0f, 0.0f));
 	model = glm::rotate(model, glm::radians(15.0f), vec3(0.0f, 1.0f, 0.0f));
 	projection = mat4(1.0f);
-	prog.setUniform("Light.Position", view * glm::vec4(5.0f, 5.0f, 2.0f, 1.0f));
-	prog.setUniform("Light.Ld", vec3(1.0f, 1.0f, 1.0f));
-	prog.setUniform("Light.La", vec3(0.4f, 0.4f, 0.4f));
-	prog.setUniform("Light.Ls", vec3(1.0f, 1.0f, 1.0f));
 
-	prog.setUniform("Material.Kd", vec3(0.2f, 0.55f, 0.9f));
-	prog.setUniform("Material.Ka", vec3(0.2f, 0.55f, 0.9f));
-	prog.setUniform("Material.Ks", vec3(0.8f, 0.8f, 0.8f));
-	prog.setUniform("Material.Shininess", 100.0f);
+	float x, z;
+	for (int i = 0; i < 3; i++) {
+		std::stringstream name;
+		name << "lights[" << i << "].Position";
+		x = 2.0f * cosf((glm::two_pi<float>() /3) * i);
+		z = 2.0f * sinf((glm::two_pi<float>() /3) * i);
+		prog.setUniform(name.str().c_str(), view * glm::vec4(x, 0.0f, z, 1.0f));
+	}
+
+	prog.setUniform("lights[0].L", vec3(0.0f, 0.0f, 0.8f));
+	prog.setUniform("lights[1].L", vec3(0.0f, 0.8f, 0.0f));
+	prog.setUniform("lights[2].L", vec3(0.8f, 0.0f, 0.0f));
+
+	prog.setUniform("lights[0].La", vec3(0.0f, 0.0f, 0.2f));
+	prog.setUniform("lights[1].La", vec3(0.0f, 0.2f, 0.0f));
+	prog.setUniform("lights[2].La", vec3(0.2f, 0.0f, 0.0f));
+
+
 }
 
 void SceneBasic_Uniform::compile()
@@ -59,6 +70,11 @@ void SceneBasic_Uniform::update( float t )
 void SceneBasic_Uniform::render()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	
+	prog.setUniform("Material.Kd", vec3(0.2f, 0.55f, 0.9f));
+	prog.setUniform("Material.Ka", vec3(0.2f, 0.55f, 0.9f));
+	prog.setUniform("Material.Ks", vec3(0.8f, 0.8f, 0.8f));
+	prog.setUniform("Material.Shininess", 100.0f);
 
 	setMatrices();
 	torus.render();
